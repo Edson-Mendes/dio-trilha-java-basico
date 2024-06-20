7ff139ef8b690963fb6f147d030dcd8eb8415c6a
Edson Mendes <edson.luiz.mendes@hotmail.com>
2024-06-20 16:42:47 -0300

    feat: add ContaTerminal

--- ContaBanco/src/main/java/br/com/emendes/ContaTerminal.java
+++ ContaBanco/src/main/java/br/com/emendes/ContaTerminal.java
@@ -1,9 +1,57 @@
 package br.com.emendes;
 
+import java.math.BigDecimal;
+import java.util.Scanner;
+
 public class ContaTerminal {
 
+  private final Scanner input = new Scanner(System.in);
+
+  /**
+   * Solicita o o nome do cliente.
+   */
+  public String getClientName() {
+    System.out.println("Por favor, digite o nome do cliente!");
+    return input.nextLine();
+  }
+
+  /**
+   * Solicita o número da agência.
+   */
+  public String getBranchNumber() {
+    System.out.println("Por favor, digite o número da agência!");
+    return input.nextLine();
+  }
+
+  /**
+   * Solicita o número da conta.
+   */
+  public int getAccountNumber() {
+    System.out.println("Por favor, digite o número da conta!");
+    return Integer.parseInt(input.nextLine());
+  }
+
+  /**
+   * Solicita o saldo da conta.
+   */
+  public BigDecimal getBalanceValue() {
+    System.out.println("Por favor, digite o saldo da conta!");
+    return new BigDecimal(input.nextLine());
+  }
+
+  /**
+   * Método main para execução da aplicação.
+   */
   public static void main(String[] args) {
-    System.out.println("Hello World!");
+    ContaTerminal contaTerminal = new ContaTerminal();
+    String clientName = contaTerminal.getClientName();
+    String branchNumber = contaTerminal.getBranchNumber();
+    int accountNumber = contaTerminal.getAccountNumber();
+    BigDecimal balanceValue = contaTerminal.getBalanceValue();
+
+    String messageTemplate = "Olá %s, obrigado por criar uma conta em nosso banco, sua agência é %s, conta %d e seu saldo %s já está disponível para saque.%n";
+
+    System.out.printf(messageTemplate, clientName, branchNumber, accountNumber, balanceValue);
   }
 
 }
---
+++ README.md
@@ -0,0 +1,3 @@
+# DIO - Trilha Java Básico - Conta Banco
+
+Projeto resposta ao desafio **Simulando uma conta bancária através do terminal/console** do [Bootcamp Santander Java Backend 2024](https://web.dio.me/track/santander-2024-backend-com-java), com o objetivo de por em prática os fundamentos da linguagem e noções de regras de estrutura de sintaxe.
